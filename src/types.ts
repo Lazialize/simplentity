8b3870f23a5a50c8ebf749504c985db6875035cf
Lazialize <lazinelia@gmail.com>
2024-04-13 19:25:22 +0900

    chore: Changed intentStyle to space
    and some rules are added.

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,10 @@
 export type Field = { type: string };
 export type Resolver<T extends { [index: string]: Field }> = {
-	[P in keyof T]: T[P]["type"] extends "string"
-		? string
-		: T[P]["type"] extends "number"
-			? number
-			: T[P]["type"] extends "boolean"
-				? boolean
-				: never;
+  [P in keyof T]: T[P]["type"] extends "string"
+    ? string
+    : T[P]["type"] extends "number"
+      ? number
+      : T[P]["type"] extends "boolean"
+        ? boolean
+        : never;
 };
