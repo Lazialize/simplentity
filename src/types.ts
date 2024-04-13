a5ccaf38fcd98b6fff1c033c52ecd051e55f449e
Lazialize <lazinelia@gmail.com>
2024-04-13 20:10:41 +0900

    feat: Added field helper functions for general types

--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,19 @@ export type Resolver<T extends { [index: string]: Field }> = {
         ? boolean
         : never;
 };
+
+type StringField = Field & { type: "string" };
+type NumberField = Field & { type: "number" };
+type BooleanField = Field & { type: "boolean" };
+
+export const string = (): StringField => {
+  return { type: "string" };
+};
+
+export const number = (): NumberField => {
+  return { type: "number" };
+};
+
+export const boolean = (): BooleanField => {
+  return { type: "boolean" };
+};
