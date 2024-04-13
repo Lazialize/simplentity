cde99549fea1f85725c736e73ab7ae878227aff3
Lazialize <lazinelia@gmail.com>
2024-04-13 18:27:21 +0900

    feat: Added general types of support.

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,10 @@
 export type Field = { type: string };
 export type Resolver<T extends { [index: string]: Field }> = {
-	[P in keyof T]: T[P]["type"] extends "string" ? string : never;
+	[P in keyof T]: T[P]["type"] extends "string"
+		? string
+		: T[P]["type"] extends "number"
+			? number
+			: T[P]["type"] extends "boolean"
+				? boolean
+				: never;
 };
